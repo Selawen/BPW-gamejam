7953c66195696bfcd089bdc37efaca8c0805bda8
Selawen <37039183+Selawen@users.noreply.github.com>
2020-05-07 16:56:59 +0200

    gamejam submission
    the one submitted to the BPW gamejam

--- BPW Gamejam/Assets/Scripts/RevealPlatform.cs
+++ BPW Gamejam/Assets/Scripts/RevealPlatform.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class RevealPlatform : MonoBehaviour
 {
     GameObject platform;
+    MeshRenderer mesh;
 
     private void Start()
     {
         platform = GameObject.Find("revealingPlatform");
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        platform.transform.position = platform.transform.position + new Vector3(0, 2.0f, 0);
+        platform.transform.position = platform.transform.position + new Vector3(0, 10.0f, 0);
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        platform.transform.position = platform.transform.position + new Vector3(0, -2.0f, 0);
+        platform.transform.position = platform.transform.position + new Vector3(0, -10.0f, 0);
+
     }
 }
