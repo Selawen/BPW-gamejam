8457332ff975956e4633bb50f2631c52fd942489
Selawen <37039183+Selawen@users.noreply.github.com>
2020-05-07 14:59:10 +0200

    added game
    Handig

---
+++ BPW Gamejam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private GameObject activatonObj;
+    private SwitchPlayer activationScript;
+
+    private int followedPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player1");
+        followedPlayer = 1;
+
+        activatonObj = GameObject.Find("SwitchPlayer");
+        activationScript = activatonObj.GetComponent<SwitchPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int activePlayer = activationScript.activePlayer;
+        if (followedPlayer == activePlayer)
+        {
+            if (Vector3.Distance(new Vector3(player.transform.position.x, 0, player.transform.position.z), new Vector3(transform.position.x, 0, transform.position.z - 10)) > 1)
+            {
+                transform.position += (new Vector3(player.transform.position.x, 0, player.transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z + 10)) * Time.deltaTime;
+            }
+        }
+        else
+        {
+            player = GameObject.Find("player" + activePlayer);
+            followedPlayer = activePlayer;
+        }
+    }
+}
---
+++ BPW Gamejam/Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour
+{
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public int activePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activePlayer = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("end"))
+        {
+            if (activePlayer == 1)
+            {
+                p1.GetComponent<Movement>().enabled = false;
+                p2.GetComponent<Movement>().enabled = true;
+                activePlayer = 2;
+            }
+            else if (activePlayer == 2)
+            {
+                p2.GetComponent<Movement>().enabled = false;
+                p3.GetComponent<Movement>().enabled = true;
+                activePlayer = 3;
+            }
+            else if (activePlayer == 3)
+            {
+                p3.GetComponent<Movement>().enabled = false;
+                p1.GetComponent<Movement>().enabled = true;
+                activePlayer = 1;
+
+            }
+        }
+    }
+}
